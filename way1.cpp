378544b32fe7a0dd125fbea58f9f9ccb28f4ee7a
Lance Chao <41135423+lance27866854@users.noreply.github.com>
2018-10-07 21:24:11 +0800

    Update way1.cpp

--- way1.cpp
+++ way1.cpp
@@ -1,90 +1,3 @@
-/*#include <iostream>
-#include <fstream>
-#include <string>
-#include <time.h>
-#include <windows.h>
-#include <stdio.h>
-
-std::string in_file_name="test_case.txt";
-std::string out_file_name="output.txt";
-std::string out_file_time_name="_time.txt";
-
-void func();
-
-int main(void){
-    for(int i=0;i<100;i++) func();
-    return 0;
-}
-void func(){
-    std::ofstream out_file_time;
-    out_file_time.open(out_file_time_name,std::ios::app);
-
-    ///start clock.
-    LARGE_INTEGER t1, t2, ts;
-    QueryPerformanceFrequency(&ts);
-    QueryPerformanceCounter(&t1);
-
-    ///file settings.
-    std::ifstream in_file;
-    std::ofstream out_file;
-    in_file.open(in_file_name,std::ios::in);
-    out_file.open(out_file_name,std::ios::out);
-
-    ///get inputs.
-    int first[101], second[101], third[101];
-    int *first_row = first, *second_row = second, *third_row = third;
-    int sum=0;
-    int x[10000], y[10000];
-    int row, col;
-    in_file>>row>>col;
-
-    for(int i=1;i<=col;i++){
-        in_file>>first_row[i];
-    }
-    for(int i=1;i<=col;i++){
-        int comp;
-        in_file>>comp;
-        if((first_row[i]>=comp)&&(i>=col||first_row[i]>=first_row[i+1])&&(i<=1||first_row[i]>=first_row[i-1])){
-            x[sum]=1;//adjust
-            y[sum]=i;
-            sum++;
-        }
-        second_row[i]=comp;
-    }
-    for(int i=2;i<row;i++){
-        for(int j=1;j<=col;j++){
-            int comp;
-            in_file>>comp;
-            if((second_row[j]>=comp)&&(second_row[j]>=first_row[j])&&(j>=col||second_row[j]>=second_row[j+1])&&(j<=1||second_row[j]>=second_row[j-1])){
-                x[sum]=i;
-                y[sum]=j;
-                sum++;
-            }
-            third_row[j]=comp;
-        }
-        //swap
-        first_row=second_row;
-        second_row=third_row;
-        third_row=first_row;
-    }
-    for(int i=1;i<=col;i++){
-        if((second_row[i]>=first_row[i])&&(i>=col||second_row[i]>=second_row[i+1])&&(i<=1||second_row[i]>=second_row[i-1])){
-            x[sum]=col;
-            y[sum]=i;
-            sum++;
-        }
-    }
-
-    ///output.
-    out_file<<sum<<std::endl;
-    for(int i=0;i<sum;i++){
-        out_file<<x[i]<<" "<<y[i]<<std::endl;
-    }
-    QueryPerformanceCounter(&t2);
-    std::cout<<"Execution Time : "<<(t2.QuadPart-t1.QuadPart)/(double)(ts.QuadPart)<<" s."<<std::endl;
-    out_file_time<<(t2.QuadPart-t1.QuadPart)/(double)(ts.QuadPart)<<"\n";
-}
-*/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -94,23 +7,12 @@ void func(){
 
 #define MAX_MATRIXSIZE 1000
 
-std::string in_file_name="matrix.data";
-std::string out_file_name="final.peak";
-std::string out_file_time_name="_time.data";
+std::string in_file_name="./106000103/matrix.data";
+std::string out_file_name="./106000103/final.peak";
 
-void func();
+int cordinate[MAX_MATRIXSIZE*MAX_MATRIXSIZE];
 
 int main(void){
-    for(int i=0;i<1;i++) func();
-    return 0;
-}
-
-//function defined here
-int cordinate[MAX_MATRIXSIZE*MAX_MATRIXSIZE];//too big
-void func(){
-    std::ofstream out_file_time;
-    out_file_time.open(out_file_time_name,std::ios::app);
-
     ///start clock.
     LARGE_INTEGER t1, t2, ts;
     QueryPerformanceFrequency(&ts);
@@ -129,52 +31,91 @@ void func(){
     int stop_pt[MAX_MATRIXSIZE];
     int row, col;
     in_file>>row>>col;
-
-    for(int i=1;i<=col;i++){
-        in_file>>first_row[i];
-    }
-    for(int i=1;i<=col;i++){//examine row 1
-        int comp;
-        in_file>>comp;
-        if((first_row[i]>=comp)&&(i>=col||first_row[i]>=first_row[i+1])&&(i<=1||first_row[i]>=first_row[i-1])){
-            cordinate[sum]=i+1;//adjust
-            sum++;
+    //row == 1
+    if(row == 1){
+        for(int i=1;i<=col;i++){
+            in_file>>first_row[i];
         }
-        second_row[i]=comp;
-    }
-    stop_pt[1]=sum;
-
-    for(int i=2;i<row;i++){//examine row 2 to row n
-        for(int j=1;j<=col;j++){
-            int comp;
-            in_file>>comp;
-            if((second_row[j]>=comp)&&(second_row[j]>=first_row[j])&&(j>=col||second_row[j]>=second_row[j+1])&&(j<=1||second_row[j]>=second_row[j-1])){
-                cordinate[sum]=i+j;
+        for(int i=1;i<=col;i++){//examine row 1
+            if((i>=col||first_row[i]>=first_row[i+1])&&(i<=1||first_row[i]>=first_row[i-1])){
+                cordinate[sum]=i+1;
                 sum++;
             }
-            third_row[j]=comp;
         }
-        //swap
-        first_row=second_row;
-        second_row=third_row;
-        third_row=first_row;
-        stop_pt[i]=sum;
+        stop_pt[1]=sum;
     }
-    for(int i=1;i<=col;i++){
-        if((second_row[i]>=first_row[i])&&(i>=col||second_row[i]>=second_row[i+1])&&(i<=1||second_row[i]>=second_row[i-1])){
-            cordinate[sum]=col+i;
-            sum++;
+    //row == 2
+    else if(row == 2){
+        for(int i=1;i<=col;i++){
+            in_file>>first_row[i];
+        }
+        for(int i=1;i<=col;i++){//examine row 1
+            int comp;
+            in_file>>comp;
+            if((first_row[i]>=comp)&&(i>=col||first_row[i]>=first_row[i+1])&&(i<=1||first_row[i]>=first_row[i-1])){
+                cordinate[sum]=i+1;//adjust
+                sum++;
+            }
+            second_row[i]=comp;
+        }
+        stop_pt[1]=sum;
+        for(int i=1;i<=col;i++){
+            if((second_row[i]>=first_row[i])&&(i>=col||second_row[i]>=second_row[i+1])&&(i<=1||second_row[i]>=second_row[i-1])){
+                cordinate[sum]=i+2;//adjust
+                sum++;
+            }
+        }
+    }
+    //row >= 3
+    else if(row >= 3){
+        for(int i=1;i<=col;i++){
+            in_file>>first_row[i];
+        }
+        for(int i=1;i<=col;i++){//examine row 1
+            int comp;
+            in_file>>comp;
+            if((first_row[i]>=comp)&&(i>=col||first_row[i]>=first_row[i+1])&&(i<=1||first_row[i]>=first_row[i-1])){
+                cordinate[sum]=i+1;//adjust
+                sum++;
+            }
+            second_row[i]=comp;
+        }
+        stop_pt[1]=sum;
+
+        for(int i=2;i<row;i++){//examine row 2 to row n
+            for(int j=1;j<=col;j++){
+                int comp;
+                in_file>>comp;
+                if((second_row[j]>=comp)&&(second_row[j]>=first_row[j])&&(j>=col||second_row[j]>=second_row[j+1])&&(j<=1||second_row[j]>=second_row[j-1])){
+                    cordinate[sum]=i+j;
+                    sum++;
+                }
+                third_row[j]=comp;
+            }
+            //swap
+            first_row=second_row;
+            second_row=third_row;
+            third_row=first_row;
+            stop_pt[i]=sum;
+        }
+        for(int i=1;i<=col;i++){
+            if((second_row[i]>=first_row[i])&&(i>=col||second_row[i]>=second_row[i+1])&&(i<=1||second_row[i]>=second_row[i-1])){
+                cordinate[sum]=col+i;
+                sum++;
+            }
         }
     }
 
     ///output.
     out_file<<sum<<"\n";
-    int diff=1;
-    for(int i=0;i<sum;i++){
-        if(i == stop_pt[diff]){diff++;}
-        out_file<<diff<<" "<<cordinate[i]-diff<<"\n";
+    stop_pt[0]=0;
+    stop_pt[row]=sum;
+    for(int i=1;i<=row;i++){
+        for(int j=stop_pt[i-1];j<stop_pt[i];j++){
+            out_file<<i<<" "<<cordinate[j]-i<<"\n";
+        }
     }
     QueryPerformanceCounter(&t2);
     std::cout<<"Execution Time : "<<(t2.QuadPart-t1.QuadPart)/(double)(ts.QuadPart)<<" s."<<"\n";
-    out_file_time<<(t2.QuadPart-t1.QuadPart)/(double)(ts.QuadPart)<<"\n";
+    return 0;
 }
